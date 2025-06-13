0c504c3b5c0c6d1541a231f7caae089435b22116
Lianne Gabriel <lgabriel2@academic.rrc.ca>
2025-06-12 21:20:51 -0500

    outputting dynamic values

--- nextjs/demo_3/starting-project/src/components/Post.jsx
+++ nextjs/demo_3/starting-project/src/components/Post.jsx
@@ -1,6 +1,10 @@
+const names = ['Lianne', 'Manuel'];
+
 function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
   return <div>
-    <p>Lianne</p>
+    <p>{chosenName}</p>
     <p>React.js is awesome!</p>
   </div>
 }
