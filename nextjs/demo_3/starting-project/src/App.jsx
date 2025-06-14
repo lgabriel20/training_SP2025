705277a2d5b8958c0f0067acc99a2daa1389c445
Lianne Gabriel <lgabriel2@academic.rrc.ca>
2025-06-13 22:21:59 -0500

    passing data using props

--- nextjs/demo_3/starting-project/src/App.jsx
+++ nextjs/demo_3/starting-project/src/App.jsx
@@ -3,9 +3,8 @@ import Post from "./components/Post";
 function App() {
   return (
     <main>
-      <Post />
-      <Post />
-      <Post />
+      <Post author="Lianne" body="React.js is awesome!"/>
+      <Post author="Lianne" body="Check out the full course!"/>
     </main>
   );
 }
