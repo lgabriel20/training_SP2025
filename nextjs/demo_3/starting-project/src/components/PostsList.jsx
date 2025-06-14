2fd740eb50476ed114bd8e8b63a8a7e0d102485d
Lianne Gabriel <lgabriel2@academic.rrc.ca>
2025-06-14 14:15:22 -0500

    prep app for state management

--- nextjs/demo_3/starting-project/src/components/PostsList.jsx
+++ nextjs/demo_3/starting-project/src/components/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from './PostsList.module.css';
 
 function PostsList(){
   return (
-  <ul className={classes.posts}>
-    <Post author="Lianne" body="React.js is awesome!"/>
-    <Post author="Sandy" body="Check out the full course!"/>
-  </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Lianne" body="React.js is awesome!"/>
+        <Post author="Sandy" body="Check out the full course!"/>
+      </ul>
+  </>
   );
 }
 
