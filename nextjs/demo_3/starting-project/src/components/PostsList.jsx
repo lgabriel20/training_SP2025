a2ae03b76b3ab5290a77414e1e037ff012ff1fcd
Lianne Gabriel <lgabriel2@academic.rrc.ca>
2025-06-14 20:36:48 -0500

    special children prop

--- nextjs/demo_3/starting-project/src/components/PostsList.jsx
+++ nextjs/demo_3/starting-project/src/components/PostsList.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import NewPost from "./NewPost";
 import Post from "./Post";
+import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostsList(){
@@ -17,12 +18,18 @@ function PostsList(){
 
   return (
     <>
-      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+      <Modal>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandler}
+        />
+      </Modal>
+
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody}/>
         <Post author="Sandy" body="Check out the full course!"/>
       </ul>
-  </>
+    </>
   );
 }
 
