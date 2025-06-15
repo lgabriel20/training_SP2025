7b02c49f489fbd86388943dc4d1ec4cc5d76bfb0
Lianne Gabriel <lgabriel2@academic.rrc.ca>
2025-06-14 21:09:20 -0500

    state & conditional content

--- nextjs/demo_3/starting-project/src/components/PostsList.jsx
+++ nextjs/demo_3/starting-project/src/components/PostsList.jsx
@@ -5,8 +5,13 @@ import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostsList(){
+  const [ modalIsVisible, setModalIsVisible ] = useState(true);
   const [enteredBody, setEnteredBody] = useState('');
   const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -16,15 +21,28 @@ function PostsList(){
     setEnteredAuthor(event.target.value);
   }
 
+  let modalContent;
+
+  // if (modalIsVisible) {
+  //   modalContent =
+  //   <Modal onClose={hideModalHandler}>
+  //     <NewPost
+  //       onBodyChange={bodyChangeHandler}
+  //       onAuthorChange={authorChangeHandler}
+  //     />
+  //   </Modal>
+  // }
+
   return (
     <>
-      <Modal>
-        <NewPost
-          onBodyChange={bodyChangeHandler}
-          onAuthorChange={authorChangeHandler}
-        />
-      </Modal>
-
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody}/>
         <Post author="Sandy" body="Check out the full course!"/>
