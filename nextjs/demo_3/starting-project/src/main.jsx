3e640a6ad031c8d84ee406dc1749b7657929c1b9
Lianne Gabriel <lgabriel2@academic.rrc.ca>
2025-06-15 20:15:31 -0500

    added layout routes

--- nextjs/demo_3/starting-project/src/main.jsx
+++ nextjs/demo_3/starting-project/src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import RootLayout from './routes/RootLayout';
 import NewPost from './components/NewPost';
 
 const router = createBrowserRouter([
-  { path: '/', element: <App /> },
-  { path: '/create-post', element: <NewPost /> }
+  { path: '/',
+    element: <RootLayout/>,
+    children: [
+      { path: '/', element: <App /> },
+      { path: '/create-post', element: <NewPost /> },
+    ]
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
