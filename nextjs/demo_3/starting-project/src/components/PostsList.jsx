7a2161c6f03a8f30b3a382a6904c5531fdc3d5b5
Lianne Gabriel <lgabriel2@academic.rrc.ca>
2025-06-15 21:25:58 -0500

    data fetching using loader

--- nextjs/demo_3/starting-project/src/components/PostsList.jsx
+++ nextjs/demo_3/starting-project/src/components/PostsList.jsx
@@ -1,24 +1,21 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import Post from "./Post";
 import classes from './PostsList.module.css';
+import { useLoaderData } from 'react-router-dom';
 
 function PostsList(){
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
+  const posts = useLoaderData();
 
   // useEffect(() => {}, []);
-  useEffect(() => {
-    async function fetchPosts() {
-      setIsFetching(true);
-      const response = await fetch('http://localhost:8080/posts');
-      const resData = await response.json();
+  // useEffect(() => {
+  //   async function fetchPosts() {
+  //     setIsFetching(true);
+  //     setPosts(resData.posts);
+  //     setIsFetching(false);
+  //     }
 
-      setPosts(resData.posts);
-      setIsFetching(false);
-      }
-
-      fetchPosts();
-  }, []);
+  //     fetchPosts();
+  // }, []);
 
   function addPostHandler(postData) {
     fetch('http://localhost:8080/posts', {
@@ -33,22 +30,17 @@ function PostsList(){
 
   return (
     <>
-      {!isFetching && posts.length > 0 && (
+      {posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => ( <Post key={post.body} author={post.author} body={post.body} /> ))}
         </ul>
       )}
-      {!isFetching && posts.length === 0 && (
+      {posts.length === 0 && (
         <div style={{textAlign: 'center', color: 'white'}}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
         </div>
       )}
-      {isFetching && (
-        <div style={{textAlign: 'center', color: 'white'}}>
-          <p>Loading posts...</p>
-        </div>
-        )}
     </>
   );
 }
